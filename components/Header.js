54010835153d9b6b8b3342533a59bee0e77625b5
Saurabh Grewal <saurabhgrewal718@gmail.com>
2021-05-25 01:49:30 +0530

    headeritem componet made and stylised

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import Image from 'next/image'; 
+import HeaderItem from '../components/HeaderItem'
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon
+} from '@heroicons/react/outline'
 
 function Header() {
     return (
         <header>
-            <h1>this is the header</h1>
-            <Image
-               src='https://links.papareact.com/ua6'
-            />
+            <HeaderItem title='HOME' Icon={HomeIcon}/>
+            <HeaderItem title='Home' Icon={BadgeCheckIcon}/>
+            <HeaderItem title='Home' Icon={LightningBoltIcon}/>
+            <HeaderItem title='Home' Icon={SearchIcon}/>
+            <HeaderItem title='Home' Icon={UserIcon}/>
+            <HeaderItem title='Home' Icon={CollectionIcon}/>
+            <Image 
+                className = "object-contain"
+                width={200}
+                src='https://links.papareact.com/ua6' height={100}/>
         </header>
     )
 }
